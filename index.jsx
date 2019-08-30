558a11214e3c7b0eef8645317c11a1fa1b1ed4cb
Sergey Spasskiy <sergey.spasskiy@akvelon.com>
2019-08-30 17:09:43 +0300

    Fix comments, move everything to jsx, add neutral bg, make quiz components

--- index.jsx
+++ index.jsx
@@ -1,9 +1,65 @@
-    class Hello extends React.Component {
-        render() {
-            return <h1>Hello, React</h1>;
-        }
-    }
-    ReactDOM.render(
-        <Hello></Hello>,
-        document.getElementById("app")
+// imports for webpack
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Label/>
+        <StartMenu/>
+        <QuizMenu/>
+      </React.Fragment>
     )
+  }
+}
+
+class Label extends React.Component {
+  render() {
+    return <h1>Anime Sage</h1>
+  }
+}
+
+class StartMenu extends React.Component {
+  render() {
+    return <div id="content" className="center">
+      <button>GO</button>
+      <h2>Options:</h2>
+    </div>
+  }
+}
+
+class QuizMenu extends React.Component {
+  render() {
+    return <React.Fragment>
+      {/*<CharImg/>*/}
+      <CharName/>
+      <AnswerButtons/>
+    </React.Fragment>
+  }
+}
+
+class AnswerButtons extends React.Component {
+  render() {
+    return <div id='ans-block' className="center">
+      <button id="1" className="answer-correct">TEST1</button>
+      <button id="2" className="">TEST2</button>
+      <button id="3" className="">TEST3</button>
+      <button id="4" className="answer-wrong">TEST4</button>
+    </div>
+  }
+}
+
+class CharName extends React.Component {
+  render() {
+    return <div id='name-next-block' className="block center">
+      <p className="char-name inline">Random Guy</p>
+      <button id="2" className="inline">NEXT ></button>
+    </div>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+);
